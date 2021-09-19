54e2c8708f97b26a8d1bbcf0003a5b100ede078a
Jonathan <jonathan.hilgart@gmail.com>
2021-09-19 10:05:32 -0700

    ftx api

--- go.mod
+++ go.mod
@@ -3,7 +3,22 @@ module github.com/jonhilgart22/go-trader
 go 1.17
 
 require (
-	github.com/gorilla/websocket v1.4.0 // indirect
-	github.com/preichenberger/go-coinbasepro/v2 v2.0.5 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/shopspring/decimal v1.2.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.0 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/go-gota/gota v0.11.0 // indirect
+	github.com/go-numb/go-ftx v0.0.0-20210814041904-1aef7a7101bb // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/klauspost/compress v1.10.7 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.21.0 // indirect
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	gonum.org/v1/gonum v0.9.1 // indirect
 )
--- go_app/main.go
+++ go_app/main.go
@@ -2,33 +2,85 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
-	coinbasepro "github.com/preichenberger/go-coinbasepro/v2"
-	"github.com/shopspring/decimal"
+	dataframe "github.com/go-gota/gota/dataframe"
+	"github.com/go-numb/go-ftx/auth"
+	"github.com/go-numb/go-ftx/rest"
+
+	// "github.com/go-numb/go-ftx/rest/private/orders"
+	// "github.com/go-numb/go-ftx/rest/private/account"
+	// "github.com/go-numb/go-ftx/rest/public/futures"
+	"github.com/go-numb/go-ftx/rest/public/markets"
+	// "github.com/go-numb/go-ftx/types"
 )
 
+func readCsvFileIntoDF(filePath string) dataframe.DataFrame {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+	df := dataframe.ReadCSV(f)
+
+	// csvReader := csv.NewReader(f)
+	// records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	return df
+}
+
 func main() {
-	client := coinbasepro.NewClient() // creds stored as environment vars, COINBASE_PRO_PASSPHRASE, COINBASE_PRO_KEY, COINBASE_PRO_SECRET
-	fmt.Println(client)
 
-	book, err := client.GetBook("BTC-USD", 1)
-	if err != nil {
-		println(err.Error())
-	}
-	fmt.Println("book = ", book)
+	client := rest.New(auth.New(os.Getenv("FTX_KEY"), os.Getenv("FTX_SECRET")))
 
-	lastPrice, err := decimal.NewFromString(book.Bids[0].Price)
+	res, err := client.Candles(&markets.RequestForCandles{
+		ProductCode: "BTC/USD",
+		Resolution:  86400,
+		Limit:       2, // optional
+		// Start:       time.Now().Add(-2500 * time.Second).Unix(), // optional
+		// End:         time.Now().Unix(),                          // optional
+	})
+	fmt.Println(res)
+
 	if err != nil {
-		println(err.Error())
+		log.Fatal(err)
 	}
-	fmt.Println(lastPrice, "last price")
-	fmt.Printf("Last price %v", lastPrice)
+
+	// client.Requ
+
+	// book, err := client.GetBook("BTC-USD", 1)
+	// if err != nil {
+	// 	println(err.Error())
+	// }
+	// fmt.Println("book = ", book)
+
+	// lastPrice, err := decimal.NewFromString(book.Bids[0].Price)
+	// if err != nil {
+	// 	println(err.Error())
+	// }
+	// fmt.Println(lastPrice, "last price")
+	// fmt.Printf("Last price %v", lastPrice)
 
 	// try pulling historical candles. This works
-	historic_rates, err := client.GetHistoricRates("BTC-USD", coinbasepro.GetHistoricRatesParams{Granularity: 86400})
-	if err != nil {
-		println(err.Error())
-	}
-	fmt.Println(historic_rates)
+	//  [ time, low, high, open, close, volume ]
+	// historic_rates, err := client.GetHistoricRates("BTC-USD", coinbasepro.GetHistoricRatesParams{Granularity: 86400})
+	// if err != nil {
+	// 	println(err.Error())
+	// }
+
+	// // loop through the historic candles
+	// for idx, entry := range historic_rates {
+	// 	fmt.Println(idx, entry)
+	// 	log.Output(1, fmt.Sprint(idx))
+	// }
+	// fmt.Println("New")
+	// open csv file, add new candles to the .csv
+	bitcoin_df := readCsvFileIntoDF("./data/historic_crypto_prices - bitcoin_jan_2017_sep_4_2021.csv")
+
+	fmt.Println(bitcoin_df)
 
 }
