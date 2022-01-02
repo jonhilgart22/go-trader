b602e70712c04cb80b993af314a0cbf4da8722b5
Jonathan <jonathan.hilgart@gmail.com>
2022-01-01 22:39:11 -0800

    update date slicing

--- app/mlcode/predict_price_movements.py
+++ app/mlcode/predict_price_movements.py
@@ -167,8 +167,10 @@ class BollingerBandsPredictor:
     def _slice_df(self):
         # some dataframes don't have enough data a full lookback window
         additional_dfs_min_date = np.max([df.index.min() for df in self.additional_dfs])
-        slice_date = np.max([self.df.index.min(), pd.to_datetime(self.from_date), additional_dfs_min_date, additional_dfs_min_date])
-        slice_date = self.df.index.min()
+        slice_date = np.max(
+            [self.df.index.min(), additional_dfs_min_date]
+        )
+
         logger.info(f"Slice date , earliest day of data for main df, = {slice_date}")
 
         self.df = self.df.loc[slice_date:, :].copy()
