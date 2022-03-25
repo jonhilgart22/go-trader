d5ffcd7678ff6c29bf7679cd18a92c9ca45b6edc
Jonathan <jonathan.hilgart@gmail.com>
2022-03-24 21:42:40 -0700

    add dollars

--- scripts/generate_performance_numbers_from_ftx.py
+++ scripts/generate_performance_numbers_from_ftx.py
@@ -1,9 +1,10 @@
-import os
 import glob
-import click
 import logging
-import ccxt
+import os
 from collections import defaultdict
+
+import ccxt
+import click
 
 logging.basicConfig(format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p", level=logging.INFO)
 
@@ -26,7 +27,7 @@ def main(directory: str, file_name_glob: str):
     list_of_coins = []
 
     for file in glob.glob(f"{directory}/*"):
-        print(file)
+        logging.info(file)
         coin = file.split("/")[1].split("_")[0]
         if coin not in list_of_coins and coin not in not_coins:
             list_of_coins.append(coin)
@@ -39,24 +40,18 @@ def main(directory: str, file_name_glob: str):
     for coin in unique_coins:
         logging.info(f"Coin = {coin}")
         ftx_key = os.getenv(coin.upper() + "_FTX_KEY")
-        print(ftx_key, 'ftx_key')
+        logging.info(ftx_key, "ftx_key")
         ftx_secret = os.getenv(coin.upper() + "_FTX_SECRET")
-        print(ftx_secret, 'ftx_secret')
+        logging.info(ftx_secret, "ftx_secret")
         logging.info(f"Subaccount name env var = {coin.upper() + '_SUBACCOUNT_NAME'}")
         subaccount_name = os.getenv(coin.upper() + "_SUBACCOUNT_NAME")
 
         logging.info(f"Subaccount name = {subaccount_name}")
 
         # make sure your version is 1.51+
-        print('CCXT Version:', ccxt.__version__)
+        logging.info("CCXT Version:", ccxt.__version__)
 
-        exchange = ccxt.ftxus({
-            'apiKey': ftx_key,
-            'secret': ftx_secret,
-
-            'FTXUS-SUBACCOUNT': subaccount_name
-
-        })
+        exchange = ccxt.ftxus({"apiKey": ftx_key, "secret": ftx_secret, "FTXUS-SUBACCOUNT": subaccount_name})
 
         # markets = exchange.load_markets()
 
@@ -69,28 +64,24 @@ def main(directory: str, file_name_glob: str):
         end_time = exchange.milliseconds()
 
         while True:
-            print('------------------------------------------------------------------')
-            params = {
-                'end_time': int(end_time / 1000),
-                'FTXUS-SUBACCOUNT': subaccount_name,
-            }
+            logging.info("------------------------------------------------------------------")
+            params = {"end_time": int(end_time / 1000), "FTXUS-SUBACCOUNT": subaccount_name}
             trades = exchange.fetch_my_trades(symbol, since, limit, params)
             if len(trades):
                 first_trade = trades[0]
-                last_trade = trades[len(trades) - 1]
-                end_time = first_trade['timestamp'] + 1000
-                print('Fetched', len(trades), 'trades from', first_trade['datetime'], 'till', last_trade['datetime'])
+                end_time = first_trade["timestamp"] + 1000
+
                 fetched_new_trades = False
                 for trade in trades:
-                    trade_id = trade['id']
+                    trade_id = trade["id"]
                     if trade_id not in all_trades:
                         fetched_new_trades = True
                         all_trades[trade_id] = trade
                 if not fetched_new_trades:
-                    print('Done')
+                    logging.info("Done")
                     break
             else:
-                print('Done')
+                logging.info("Done")
                 break
 
         all_trades = list(all_trades.values())
@@ -100,21 +91,20 @@ def main(directory: str, file_name_glob: str):
         total_lost = 0
         current_buy_price = None
 
-        print('Fetched', len(all_trades), 'trades')
+        logging.info(f"Fetched {len(all_trades)} trades")
         for i in range(0, len(all_trades)):
             trade = all_trades[i]
-            print(i, trade['id'], trade['datetime'], trade['amount'], trade['price'], trade['side'])
 
-            dollars_traded = trade['price'] * trade['amount']
+            dollars_traded = trade["price"] * trade["amount"]
 
-            if trade['side'] == 'buy':
+            if trade["side"] == "buy":
                 total_bought += dollars_traded
-                current_buy_price = trade['price']
-            if trade['side'] == 'sell':
+                current_buy_price = trade["price"]
+            if trade["side"] == "sell":
                 total_sold += dollars_traded
                 if current_buy_price is None:
                     continue
-                elif current_buy_price < trade['price']:  # buy low sell high
+                elif current_buy_price < trade["price"]:  # buy low sell high
                     total_won += 1
                 else:
                     total_lost += 1
@@ -146,5 +136,5 @@ def main(directory: str, file_name_glob: str):
     logging.info(f"Amount won or lost per trade = ${total_dollars_won_or_lost/total_trades:.2f}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
