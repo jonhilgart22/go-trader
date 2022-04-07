0fd5bc5368c1beb70b0a364ffff7a754e568e665
Jonathan <jonathan.hilgart@gmail.com>
2022-04-06 19:07:39 -0700

    revert

---
+++ app/src/pull_tbt.go
@@ -0,0 +1,13 @@
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/markcheno/go-quote"
+// )
+
+// func main() {
+// 	spy, _ := quote.NewQuoteFromYahoo("TBT", "2021-10-01", "2021-11-01", quote.Daily, true)
+// 	fmt.Print(spy.CSV())
+
+// }
