ef9743158d768db449ddbd318b778820e5966bb4
Jonathan <jonathan.hilgart@gmail.com>
2022-04-17 20:49:50 -0700

    update performance

--- scripts/generate_performance_numbers_from_ftx.py
+++ scripts/generate_performance_numbers_from_ftx.py
@@ -90,13 +90,18 @@ def main(directory: str, file_name_glob: str) -> None:
         n_buy = 0
         total_sold = 0
         n_sell = 0
+        total_fees = 0
         current_buy_price = None
 
         logging.info(f"Fetched {len(all_trades_list)} trades")
         for i in range(0, len(all_trades_list)):
-            if (i == len(all_trades_list) - 1) and (trade['side'] == 'buy'):  # last trade, if BUY, exclude.. we are in an active position
-                continue
             trade = all_trades_list[i]
+            logging.info(f"{i} of {len(all_trades_list)-1}")
+            if (i == len(all_trades_list) - 1) and (trade['side'] == 'buy'):  # last trade, if BUY, exclude.. we are in an active position
+                logging.info(f"Last trade, excluding")
+                logging.info(f"{trade['datetime'], trade['side'], trade['price'], trade['amount'],  trade['cost']}")
+                continue
+            total_fees += float(trade['fee']['cost'])
 
             logging.info(f"{trade['datetime'], trade['side'], trade['price'], trade['amount'],  trade['cost']}")
 
@@ -109,12 +114,15 @@ def main(directory: str, file_name_glob: str) -> None:
                 total_sold += dollars_traded
                 n_sell += 1
 
-        total_won_or_lost = total_bought - total_sold
+        total_won_or_lost = total_sold - total_bought
+        total_won_or_lost_minus_fees = total_won_or_lost - total_fees
         logging.info("total bought = " + str(total_bought))
         logging.info("total sold = " + str(total_sold))
         logging.info(f"Total won = {total_won_or_lost }")
+        logging.info(f"Total fees = {total_fees}")
+        logging.info(f"Total won minus fees = {total_won_or_lost_minus_fees}")
         # profit for long trades, total money at risk, # of trades, winning trades
-        performance_per_coin[coin] = (total_won_or_lost, total_bought, n_buy + n_sell, n_buy)
+        performance_per_coin[coin] = (total_won_or_lost, total_bought, n_buy + n_sell, n_buy, total_won_or_lost_minus_fees)
     logging.info(f"performance_per_coin = {performance_per_coin}")
     # aggregate metrics
 
@@ -122,19 +130,25 @@ def main(directory: str, file_name_glob: str) -> None:
     total_dollars_at_risk = 0
     n_trades_buy = 0
     total_trades = 0
+    total_won_or_lost_minus_fees = 0
+    total_fees = 0
     for key, value in performance_per_coin.items():
         total_dollars_won_or_lost += value[0]
         total_dollars_at_risk += value[1]
         n_trades_buy += value[3]
         total_trades += value[2]
+        total_won_or_lost_minus_fees += value[4]
+        total_fees += total_dollars_won_or_lost - total_won_or_lost_minus_fees
 
-    logging.info(f"total_dollars_won_or_lost = ${total_dollars_won_or_lost}")
-    logging.info(f"total_dollars_at_risk = ${total_dollars_at_risk}")
+    logging.info(f"total_dollars_won_or_lost = ${total_dollars_won_or_lost:.2f}")
+    logging.info(f"total_fees = ${total_fees:.2f}")
+    logging.info(f"total_won_or_lost_minus_fees = ${total_won_or_lost_minus_fees:.2f}")
+    logging.info(f"total_dollars_at_risk = ${total_dollars_at_risk:.2f}")
     logging.info(f"total_trades = {total_trades}")
     logging.info(f"n_trades_buy = {n_trades_buy}")
     logging.info(f"Bat rate = {n_trades_buy/total_trades * 100:.2f}%")
-    logging.info(f"Percent Return = {(total_dollars_won_or_lost/total_dollars_at_risk)*100:.2f}%")
-    logging.info(f"Amount won or lost per trade = ${total_dollars_won_or_lost/total_trades:.2f}")
+    logging.info(f"Percent return minus feeds = {(total_won_or_lost_minus_fees/total_dollars_at_risk)*100:.2f}%")
+    logging.info(f"Amount won or lost per trade minus fees = ${total_won_or_lost_minus_fees/total_trades:.2f}")
 
 
 if __name__ == "__main__":
